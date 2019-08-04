36658c4c62d3e8159c8a4b4651474b5b60f7c4d0
Fernando Baron <fernandobaron1@hotmail.com>
2019-08-03 22:12:01 -0300

    Modelo 1

---
+++ CondicoesLaranja2/src/condicoeslaranja2/CondicoesLaranja2.java
@@ -0,0 +1,27 @@
+
+package condicoeslaranja2;
+
+import java.util.Scanner;
+
+
+public class CondicoesLaranja2 {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner leia = new Scanner (System.in);
+       System.out.println ("Informe a quantidade");
+       int qtd = leia.nextInt();
+       double vlrLaranja = 0;
+       
+       if (qtd < 12) {
+           vlrLaranja = 1.10;
+       } else {
+           vlrLaranja = 0.75;
+       }
+       
+       double resultado = qtd * vlrLaranja;
+       System.out.println ("O valor total é: "+resultado);
+    }
+    
+}
